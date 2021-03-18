b64b5eff41fd0aa5f75c61ee149bae7ca94e0bc5
Chris-k-m <61823895+Chris-k-m@users.noreply.github.com>
2021-03-18 03:32:37 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 import re
-
 import MySQLdb.cursors
 import mysql.connector
-from flask import Flask, render_template, request, session, url_for
+from flask import Flask, render_template, request, session
 from flask_bootstrap import Bootstrap
 from flask_mysqldb import MySQL
 from werkzeug.security import generate_password_hash
@@ -20,7 +19,7 @@ app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = 'root'
 app.config['MYSQL_DB'] = 'online_ventures'
-app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+app.config['MYSQL_CURSOR_CLASS'] = 'DictCursor'
 
 mysql.init_app(app)
 
@@ -35,11 +34,12 @@ def login():
     # Check if "username" and "password" POST requests exist (user submitted form)
     if request.method == 'POST' and 'email' in request.form and 'password' in request.form:
         # Create variables for easy access
-        email = request.form['email']
-        password = request.form['password']
+        session['email'] = request.form['email']
+        session['password'] = request.form['password']
         # check if data is in db
         cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        cursor.execute('SELECT * FROM employee WHERE email = %s AND password =MD5(%s)', (email, password))
+        cursor.execute('SELECT password FROM password WHERE password =MD5(%s) ', (session['password'],))
+        cursor.execute('SELECT * FROM employee WHERE email = %s ', (session['email'],))
         # Fetch one record and return result
         employee = cursor.fetchone()
         # if account exists
@@ -47,7 +47,13 @@ def login():
             # Create session data, we can access this data in other routes
             session['loggedin'] = True
             session['employee_id'] = employee['employee_id']
+            session['countryid'] = employee['countryid']
+            session['departmentid'] = employee['departmentid']
+            session['designationid'] = employee['designationid']
             session['user_name'] = employee['user_name']
+            session['DOB'] = employee['DOB']
+            session['sir_name'] = employee['sir_name']
+
             # Redirect to home page
             return render_template("home.html")
         else:
@@ -55,7 +61,7 @@ def login():
             msg = 'Incorrect email/password!'
             # Show the login form with message (if any)
             return render_template('login.html', msg=msg)
-    return render_template('login.html', )
+    return render_template('login.html')
 
 
 @app.route('/online_ventures/register', methods=['GET', 'POST'])
@@ -92,8 +98,9 @@ def register():
         else:
             cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
             # Account doesnt exists and the form data is valid, now insert new account into employee table
-            cursor.execute('INSERT INTO employee VALUES (NULL, %s, %s, %s, MD5(%s), %s, %s, NULL, NULL,NULL)',
-                           (user_name, sir_name, email, password, gender, DOB))
+            cursor.execute('INSERT INTO employee VALUES (NULL, %s, %s, %s, %s, %s, NULL, NULL, NULL)',
+                           (user_name, sir_name, email, gender, DOB,))
+            cursor.execute('INSERT INTO password VALUES( MD5(%s), NULL,NULL)', (password,))
             mysql.connection.commit()
             msg = 'You have successfully registered!'
         return render_template('login.html', msg='You have successfully registered!')
@@ -146,21 +153,10 @@ def register():
 def details():
     if 'loggedin' in session:
         cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        cursor.execute('SELECT * FROM employee WHERE employee_id = % s', (session['employee_id'],))
+        cursor.execute('SELECT * FROM employee WHERE employee_id = %s', (session['employee_id'],))
         employee = cursor.fetchone()
         return render_template("details.html", employee=employee)
-    return render_template("login.html")
-
-
-@app.route('/onlineventures/logout')
-def logout():
-    # check if user is logged in:
-    # Remove session data, this will log the user out
-    session.pop('loggedin', None)
-    session.pop('employee_id', None)
-    session.pop('user_name', None)
-    # Redirect to login page
-    return render_template("login.html")
+    return render_template("details.html")
 
 
 @app.route('/onlineventures/home')
@@ -168,50 +164,74 @@ def home():
     # Check if employee is loggedin
     if 'loggedin' in session:
         # employee is loggedin show them the home page
-        return render_template('home.html', user_name=session['user_name'])
+        return render_template('home.html', user_name=(session['user_name']))
     # User is not loggedin redirect to login page
     return render_template("login.html")
 
 
 @app.route('/online_ventures/add_details', methods=['GET', 'POST'])
 def add_details():
-    msg = ''
     if 'loggedin' in session:
-        if request.method == 'POST' and 'email' in request.form and 'departmentid' in request.form and 'countryid' in request.form and 'designationid' in request.form:
-            email = request.form['email']
-            departmentid = request.form['departmentid']
-            countryid = request.form['countryid']
-            designationid = request.form['designationid']
-            cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-            cursor.execute('SELECT * FROM employee WHERE email = % s', (email,))
-            employee = cursor.fetchone()
-            if employee:
-                cursor.execute(
-                    'UPDATE employee SET email =% s, departmentid =% s, countryid =% s, designationid =% s WHERE employee_id =% s',
-                    (email, departmentid, countryid, designationid, (session['employee_id']),))
-                mysql.connection.commit()
-                msg = 'You have successfully updated !'
-                print(email, departmentid, departmentid, countryid)
-        elif request.method == 'POST':
-            msg = 'Please fill out the form !'
-        return render_template("add_details.html", msg=msg)
-    return render_template("login.html")
+        # fetch data from database
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM department')
+        departments = cursor.fetchall()
+        cursor.execute('SELECT * FROM country')
+        countries = cursor.fetchall()
+        cursor.execute('SELECT * FROM designation')
+        designations = cursor.fetchall()
+        if request.form == 'POST' and 'countryid' in request.form.get and 'departmentid' in request.form.get and 'designationid' in request.form.get:
+            countryid = request.form.get['countryid']
+            departmentid = request.get['departmentid']
+            designationid = request.form.get['designationid']
+            cursor.execute('UPDATE employee set countryid=%s, departmentid=%s, designationid=%s WHERE employee_id=%s',
+                           (countryid, departmentid, designationid, (session['employee_id'],)))
+            return render_template("add_details.html", msg='details saved')
+        else:
+            msg = 'details not yet saved'
+        return render_template("add_details.html", msg=msg, countries=countries, designations=designations,
+                               departments=departments)
+    else:
+        msg = 'details not filled'
+    return render_template("add_details.html", msg=msg)
 
-    # if request.method == 'POST' and 'countryid' in request.form and 'departmentid' in request.form and 'designatonid' in request.form:
-    #     countryid = request.form['countryid']
-    #     designationid = request.form['designationid']
-    #     departmentid = request.form['departmentid']
-    #     cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-    #     employee = cursor.fetchone()
-    #     cursor.execute = (
-    #             "UPDATE employee SET departmentid = %s,countryid = %s, designationid = %s WHERE employee_id = employee_id)",
-    #             (departmentid, countryid, designationid))
-    #     mysql.connection.commit()
-    #     msg = "update done successfully"
-    #     return render_template("details.html", msg=msg, employee=employee)
-    # else:
-    #     msg = "update error"
-    #     return render_template("add_details.html", msg=msg)
+
+# if 'logged_in' in session:
+#     cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+#     cursor.execute('SELECT * FROM employee WHERE employee_id = % s', (session['employee_id'],))
+#     cursor.execute('SELECT departmentid, department_name FROM department')
+#     cursor.execute('SELECT countryid, country_name FROM country')
+#     cursor.execute('SELECT designationid, designation_name FROM designation')
+#     departments = cursor.fetchall()
+#     countries = cursor.fetchall()
+#     designations = cursor.fetchall()
+#     employee = cursor.fetchone()
+#     cursor.execute(
+#                 'UPDATE employee SET departmentid =% s, countryid =% s, designationid =% s WHERE employee_id =% s',
+#                 (departmentid, countryid, designationid, (session['employee_id']),))
+#             mysql.connection.commit()
+#             msg = 'You have successfully updated !'
+#             return render_template("add_details.html", msg=msg, departments=departments, designations=designations,
+#                                    countries=countries)
+#     elif request.method == 'POST':
+#         msg = 'Please fill out the form !'
+#         return render_template("add_details.html", msg=msg)
+
+# if request.method == 'POST' and 'countryid' in request.form and 'departmentid' in request.form and 'designatonid' in request.form:
+#     countryid = request.form['countryid']
+#     designationid = request.form['designationid']
+#     departmentid = request.form['departmentid']
+#     cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+#     employee = cursor.fetchone()
+#     cursor.execute = (f
+#             "UPDATE employee SET departmentid = %s,countryid = %s, designationid = %s WHERE employee_id = employee_id)",
+#             (departmentid, countryid, designationid))
+#     mysql.connection.commit()
+#     msg = "update done successfully"
+#     return render_template("details.html", msg=msg, employee=employee)
+# else:
+#     msg = "update error"
+#     return render_template("add_details.html", msg=msg)
 
 
 @app.route('/onlineventures/department')
@@ -242,10 +262,10 @@ def designation():
 def delete_user():
     if 'loggedin' in session:
         cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        cursor.execute("DELETE FROM employee WHERE employee_id=%s", (session['employee_id']))
+        cursor.execute("DROP * FROM employee WHERE employee_id=%s", (session['employee_id'],))
         mysql.connection.commit()
         msg = 'User deleted successfully!'
-        return render_template('/', msg=msg)
+        return render_template('register.html', msg=msg)
 
 
 @app.route('/online_ventures/add_department', methods=['GET', 'POST'])
@@ -289,7 +309,6 @@ def add_designation():
             msg = 'designation exists'
         else:
             cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-            # Account doesnt exists and the form data is valid, now insert new account into employee table
             cursor.execute('INSERT INTO designation VALUES (%s, %s)',
                            (designationid, designation_name))
             mysql.connection.commit()
@@ -323,6 +342,118 @@ def add_country():
     return render_template("add_country.html")
 
 
+@app.route('/online_ventures/edit_department', methods=['GET', 'POST'])
+def edit_department():
+    # validate if user has input data
+    if request.method == 'POST' and "departmentid" in request.form and "department_name" in request.form:
+        new_departmentid = request.form['departmentid']
+        new_department_name = request.form['department_name']
+        # connect to mysql database
+        if 'loggedin' in session:
+            cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+            cursor.execute('SELECT departmentid FROM employee WHERE employee_id= %s', (session['employee_id'],))
+            departmentid = cursor.fetchone()
+            print(departmentid)
+            print(new_departmentid)
+            if departmentid != new_departmentid:
+                cursor.execute('UPDATE department SET department_name = %s WHERE departmentid = %s',
+                               (new_department_name, (session['departmentid'],)))
+                mysql.connection.commit()
+                msg = 'You have successfully updated !'
+                return render_template("department.html", msg=msg)
+            else:
+                msg = 'use the correct ID'
+            return render_template("edit_department.html", msg=msg)
+    else:
+        msg = 'input data!'
+        return render_template("edit_department.html", msg=msg)
+
+
+@app.route('/online_ventures/edit_designation', methods=['GET', 'POST'])
+def edit_designation():
+    # validate if user has input data
+    if request.method == 'POST' and "designationid" in request.form and "designation_name" in request.form:
+        new_designationid = request.form['designationid']
+        new_designation_name = request.form['designation_name']
+        # connect to mysql database
+        if 'loggedin' in session:
+            cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+            cursor.execute('SELECT designationid FROM employee WHERE employee_id= %s', (session['employee_id'],))
+            designationid = cursor.fetchone()
+            print(designationid)
+            print(new_designationid)
+            if designationid != new_designationid:
+                cursor.execute('UPDATE designation SET designation_name = %s WHERE designationid = %s',
+                               (new_designation_name, (session['designationid'],)))
+                mysql.connection.commit()
+                msg = 'You have successfully updated !'
+                return render_template("designation.html", msg=msg)
+            else:
+                msg = 'use the correct ID'
+            return render_template("edit_designation.html", msg=msg)
+    else:
+        msg = 'input data!'
+        return render_template("edit_designation.html", msg=msg)
+
+
+@app.route('/online_ventures/edit_country', methods=['GET', 'POST'])
+def edit_country():
+    # validate if user has input data
+    if request.method == 'POST' and "countryid" in request.form and "country_name" in request.form:
+        new_countryid = request.form['countryid']
+        new_country_name = request.form['country_name']
+        # connect to mysql database
+        if 'loggedin' in session:
+            cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+            cursor.execute('SELECT countryid FROM employee WHERE employee_id= %s', (session['employee_id'],))
+            countryid = cursor.fetchone()
+            print(countryid)
+            print(new_countryid)
+            if countryid != new_countryid:
+                cursor.execute('UPDATE country SET country_name = %s WHERE countryid = %s',
+                               (new_country_name, (session['countryid'],)))
+                mysql.connection.commit()
+                msg = 'You have successfully updated !'
+                return render_template("country.html", msg=msg)
+            else:
+                msg = 'use the correct ID'
+            return render_template("edit_country.html", msg=msg)
+    else:
+        msg = 'input data!'
+        return render_template("edit_country.html", msg=msg)
+
+
+#     if 'loggedin' in session:
+#         if request.method == 'POST' in request.form and "updatedcid" in request.form and "updatedcname" in request.form:
+#             newcountryname= request.form['updatedcname']
+#             newcountryid = request.form['updatedcid']
+#             cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+#             cursor.execute("SELECT * FROM country WHERE countryid = %s", (session['countryid']))
+#             cursor.execute("SELECT * FROM employee where employeeid = %s", (session['employeeid']))
+#             employee = cursor.fetchone()  # data from database
+#             if employee:
+#                 cursor.execute('UPDATE country SET countryid = %s, country_name =%s, WHERE employee_id =% s',
+#                                (newcountryid, newcountryname, (session['employeeid'])))
+#                 mysql.connection.commit()
+#                 msg = 'You have successfully added country!'
+#                 return render_template('country.html', msg=msg)
+#         msg = 'You havent added country yet!'
+#     return render_template("edit_country.html", msg=msg)
+
+
+@app.route('/onlineventures/logout')
+def logout():
+    # check if user is logged in:
+    if 'loggedin' in session:
+        # Remove session data, this will log the user out
+        session.pop('loggedin', None)
+        session.pop('employee_id', None)
+        session.pop('user_name', None)
+        session.clear()
+        # Redirect to login page
+    return render_template("login.html")
+
+
 @app.errorhandler(404)
 def page_not_found(e):
     return "page not found", 404
@@ -332,5 +463,6 @@ def page_not_found(e):
 def internal_error(e):
     return "internal error", 500
 
-# if __name__ == "__main__":
-#     app.run(debug=True)
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- tables.py
+++ tables.py
@@ -3,7 +3,7 @@ import mysql.connector
 db = mysql.connector.connect(host="localhost",
                              user="root",
                              password="root",
-                             database="onlineventures"
+                             database="online_ventures"
                              )
 cursor = db.cursor()
 
@@ -13,12 +13,16 @@ cursor = db.cursor()
 
 # cursor.executemany(sqlform, employees)
 # cursor.execute("ALTER TABLE employee ADD COLUMN designation CHAR(15) AFTER  id, ADD COLUMN department_name CHAR(100) NOT NULL,ADD COLUMN country CHAR(100) NOT NULL,ADD COLUMN gender CHAR(10) NOT NULL")
-cursor.execute("CREATE TABLE department( department_name CHAR(20), userid int, FOREIGN KEY (userid) REFERENCES employee(id))")
+# cursor.execute("CREATE TABLE department( department_name CHAR(20), userid int, FOREIGN KEY (userid) REFERENCES employee(id))")
 # cursor.execute("CREATE TABLE gender( male CHAR(10),userid int, FOREIGN KEY (userid) REFERENCES employee(id))")
 # cursor.execute("CREATE TABLE country ( country_name CHAR (100), userid int, FOREIGN KEY (userid) REFERENCES employee(id))")
 # cursor.execute("CREATE TABLE designation( location CHAR(100), userid int, FOREIGN KEY (userid) REFERENCES employee(id))")
-# cursor.execute("ALTER TABLE employee DROP COLUMN username")
 
+# cursor.execute("CREATE TABLE password ( password VARCHAR (250), password_id int PRIMARY KEY AUTO_INCREMENT)")
+# cursor.execute("ALTER TABLE password MODIFY password_id VARCHAR(250) AUTO_INCREMENT PRIMARY KEY")
+
+# cursor.execute('drop table password')
+cursor.execute('ALTER TABLE password ADD CONSTRAINT fk_password_emp FOREIGN KEY (employee_id) REFERENCES employee (employee_id)')
 
 
 
